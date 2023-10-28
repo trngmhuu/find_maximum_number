b9e37ac73956c69fcc1f6f65f39367038a5c3b64
Truong Minh Huu <99800511+trngmhuu@users.noreply.github.com>
2023-10-28 20:45:52 +0700

    Thêm file MaxNumber.java
    Hàm tìm số lớn nhất trả về biến max có kiểu dữ liệu là Integer

---
+++ MaxNumber.java
@@ -0,0 +1,31 @@
+package test;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 3, 4, 5};
+        
+        // Gọi hàm findMax và in kết quả
+        int max = findMax(arr);
+        System.out.println("Số lớn nhất trong mảng là: " + max);
+	}
+	
+	// Hàm tìm số lớn nhất trong mảng
+    public static int findMax(int[] arr) {
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("Mảng rỗng, không có số lớn nhất để tìm.");
+        }
+
+        int max = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        
+        return max;
+    }
+
+}
